6333db7e337d3b169dd7b0bcab968e9edb02e784
vasanabria@uned.cr <vasanabria@uned.cr>
2020-11-13 20:57:02 -0600

    Se agrega Clase Datos

---
+++ Datos/Datos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.Entidades;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class Datos
+    {
+        public List<Clientes> ObtenerClientes()
+        {
+            List<Clientes> resultado = new List<Clientes>();
+            string cadena = ConfigurationManager.ConnectionStrings["conexionGota"].ConnectionString;
+            SqlConnection conexionSQL = new SqlConnection(cadena);
+            SqlCommand comandoSQL = new SqlCommand();
+            SqlDataReader lectorSQL;
+
+            string sentenciaSQL = "select IDENTIFICACION, NOMBRE, APELLIDO1, APELLIDO2, CORREOELECTRONICO,NUMCELULAR from CLIENTES;";
+
+            comandoSQL.CommandType = CommandType.Text;
+            comandoSQL.CommandText = sentenciaSQL;
+            comandoSQL.Connection = conexionSQL;
+
+            conexionSQL.Open();
+            lectorSQL = comandoSQL.ExecuteReader();
+
+            while (lectorSQL.Read())
+            {
+                Clientes item = new Clientes();
+                item.IDENTIFICACION = lectorSQL.GetString(0);
+                item.NOMBRE = lectorSQL.GetString(1);
+                item.APELLIDO1 = lectorSQL.GetString(2);
+                item.APELLIDO2 = lectorSQL.GetString(3);
+                item.CORREOELECTRONICO = lectorSQL.GetString(4);
+                item.NUMCELULAR = lectorSQL.GetString(5);
+                resultado.Add(item);
+
+            }
+            conexionSQL.Close();
+            return resultado;
+        }
+
+
+
+
+    }
+}
--- La Gota/Program.cs
+++ La Gota/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using 
 
 namespace La_Gota
 {
@@ -14,6 +15,8 @@ namespace La_Gota
         [STAThread]
         static void Main()
         {
+            
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMenu());
